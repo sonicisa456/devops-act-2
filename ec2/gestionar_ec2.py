da9f9b31faac429e9ad68bbda383642a8485a7aa
EC2 Default User <ec2-user@ip-172-31-76-227.ec2.internal>
2026-04-01 23:38:33 +0000

    feat: manejo de errores

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -14,23 +14,23 @@ def listar_instancias():
             print(f"ID: {instance_id}")
             print(f"Estado: {estado}")
             print("-" * 20)
-	    print("todo bien :)")
+            print("todo bien :)")
 
 
 def iniciar_instancia(instance_id):
-    ec2.start_instances(InstanceIds=[instance_id])
-    print(f"Instancia {instance_id} iniciada")
-
-
-if not instance_id:
-    print("Error: Debes proporcionar un ID de instancia")
-
+    try:
+        ec2.start_instances(InstanceIds=[instance_id])
+        print(f"Instancia {instance_id} iniciada")
+    except Exception as e:
+        print("Error al iniciar:", e)
 
 
 def detener_instancia(instance_id):
-    ec2.stop_instances(InstanceIds=[instance_id])
-    print(f"Instancia {instance_id} detenida")
-
+    try:
+        ec2.stop_instances(InstanceIds=[instance_id])
+        print(f"Instancia {instance_id} detenida")
+    except Exception as e:
+        print("Error al detener:", e)
 
 
 if __name__ == "__main__":
