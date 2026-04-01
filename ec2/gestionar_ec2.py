2fc38d5839ef91e0664f5a79d0d71c4698a565f5
EC2 Default User <ec2-user@ip-172-31-76-227.ec2.internal>
2026-04-01 23:17:42 +0000

    feat: script para listar instancias EC2 con boto3

---
+++ ec2/gestionar_ec2.py
@@ -0,0 +1,19 @@
+import boto3
+
+# Crear cliente de EC2
+ec2 = boto3.client('ec2', region_name='us-east-1')
+
+def listar_instancias():
+    response = ec2.describe_instances()
+
+    for reserva in response['Reservations']:
+        for instancia in reserva['Instances']:
+            instance_id = instancia['InstanceId']
+            estado = instancia['State']['Name']
+
+            print(f"ID: {instance_id}")
+            print(f"Estado: {estado}")
+            print("-" * 20)
+
+if __name__ == "__main__":
+    listar_instancias()
