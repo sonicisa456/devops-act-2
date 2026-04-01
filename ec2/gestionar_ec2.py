baa6f958e2043fdb6cdd7989a3294ab2f2616fcc
EC2 Default User <ec2-user@ip-172-31-76-227.ec2.internal>
2026-04-01 23:28:01 +0000

    feat: iniciar instancia EC2

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -16,5 +16,11 @@ def listar_instancias():
             print("-" * 20)
 	    print("todo bien :)")
 
+
+def iniciar_instancia(instance_id):
+    ec2.start_instances(InstanceIds=[instance_id])
+    print(f"Instancia {instance_id} iniciada")
+
+
 if __name__ == "__main__":
     listar_instancias()
