706014c895bbbbe024683cb210393a8fd7253849
EC2 Default User <ec2-user@ip-172-31-76-227.ec2.internal>
2026-04-03 02:41:52 +0000

    feat: script EC2 con parámetros CLI

--- ec2/gestionar_ec2.py
+++ ec2/gestionar_ec2.py
@@ -1,20 +1,17 @@
 import boto3
+import sys
 
-# Crear cliente de EC2
 ec2 = boto3.client('ec2', region_name='us-east-1')
+
 
 def listar_instancias():
     response = ec2.describe_instances()
 
     for reserva in response['Reservations']:
         for instancia in reserva['Instances']:
-            instance_id = instancia['InstanceId']
-            estado = instancia['State']['Name']
-
-            print(f"ID: {instance_id}")
-            print(f"Estado: {estado}")
+            print("ID:", instancia['InstanceId'])
+            print("Estado:", instancia['State']['Name'])
             print("-" * 20)
-            print("todo bien :)")
 
 
 def iniciar_instancia(instance_id):
@@ -33,5 +30,45 @@ def detener_instancia(instance_id):
         print("Error al detener:", e)
 
 
+def terminar_instancia(instance_id):
+    try:
+        ec2.terminate_instances(InstanceIds=[instance_id])
+        print(f"Instancia {instance_id} terminada")
+    except Exception as e:
+        print("Error al terminar:", e)
+
+
 if __name__ == "__main__":
-    listar_instancias()
+    if len(sys.argv) < 2:
+        print("Uso:")
+        print("python3 gestionar_ec2.py listar")
+        print("python3 gestionar_ec2.py iniciar <id>")
+        print("python3 gestionar_ec2.py detener <id>")
+        print("python3 gestionar_ec2.py terminar <id>")
+        sys.exit(1)
+
+    comando = sys.argv[1]
+
+    if comando == "listar":
+        listar_instancias()
+
+    elif comando == "iniciar":
+        if len(sys.argv) < 3:
+            print("Debes proporcionar el ID de la instancia")
+        else:
+            iniciar_instancia(sys.argv[2])
+
+    elif comando == "detener":
+        if len(sys.argv) < 3:
+            print("Debes proporcionar el ID de la instancia")
+        else:
+            detener_instancia(sys.argv[2])
+
+    elif comando == "terminar":
+        if len(sys.argv) < 3:
+            print("Debes proporcionar el ID de la instancia")
+        else:
+            terminar_instancia(sys.argv[2])
+
+    else:
+        print("Comando no válido")
